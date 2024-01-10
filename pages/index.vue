34fd7a597c7f620df8ddc4634cbf0b4d59f61f87
Misbah Ansori <misbah.ansori24@gmail.com>
2024-01-10 10:48:35 +0700

    Feat : add prettier config

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
-    <aside>
-      <Button size="sm">Menu</Button>
+    <aside class="flex min-h-screen w-64 flex-col border-r">
+      <div class="mx-2 flex flex-col items-start gap-1 px-2 py-4">
+        <Button size="sm" class="w-full justify-start">Dashboard</Button>
+        <Button size="sm" variant="ghost" class="w-full justify-start">
+          Chart
+        </Button>
+        <Button size="sm" variant="ghost" class="w-full justify-start">
+          Payments
+        </Button>
+      </div>
     </aside>
   </div>
 </template>
---
+++ prettier.config.cjs
@@ -0,0 +1,5 @@
+// prettier.config.js
+module.exports = {
+  htmlWhitespaceSensitivity: "ignore",
+  plugins: ["prettier-plugin-tailwindcss"],
+};
