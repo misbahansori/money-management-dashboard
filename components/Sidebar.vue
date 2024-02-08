9e7f316b65c2df00982eb63a731121d14daa8871
Misbah Ansori <misbah.ansori24@gmail.com>
2024-02-08 17:25:16 +0700

    wip

---
+++ components/Sidebar.vue
@@ -0,0 +1,141 @@
+<script setup lang="ts">
+import {
+  ArchiveIcon,
+  BellRingIcon,
+  LayoutGridIcon,
+  MessageCircleQuestionIcon,
+  MessageSquareTextIcon,
+  PackageIcon,
+  SettingsIcon,
+  ShoppingBagIcon,
+  StoreIcon,
+  Wallet2Icon,
+} from "lucide-vue-next";
+
+const navigations = [
+  {
+    title: "overview",
+    children: [
+      {
+        title: "Dashboard",
+        icon: LayoutGridIcon,
+        url: "/dashboard",
+      },
+      {
+        title: "Delivery",
+        icon: ArchiveIcon,
+        url: "/delivery",
+      },
+      {
+        title: "Orders",
+        icon: ShoppingBagIcon,
+        url: "/orders",
+      },
+      {
+        title: "Products",
+        icon: PackageIcon,
+        url: "/products",
+      },
+      {
+        title: "Finance",
+        icon: Wallet2Icon,
+        url: "/finance",
+      },
+    ],
+  },
+  {
+    title: "account",
+    children: [
+      {
+        title: "Store",
+        icon: StoreIcon,
+        url: "/store",
+      },
+      {
+        title: "Settings",
+        icon: SettingsIcon,
+        url: "/settings",
+      },
+      {
+        title: "Notification",
+        icon: BellRingIcon,
+        url: "/notification",
+      },
+    ],
+  },
+];
+
+const activeNavigation = computed(() => {
+  return "/dashboard";
+});
+</script>
+
+<template>
+  <aside
+    class="fixed inset-y-0 left-0 flex min-h-screen w-[280px] flex-col border-r py-2"
+  >
+    <div class="mx-2 flex h-20 items-center px-2">
+      <Button
+        variant="ghost"
+        class="flex h-auto flex-1 items-center justify-between gap-2 py-2"
+      >
+        <Avatar variant="circle" size="sm">
+          <AvatarImage src="/img/avatar.png" />
+          <AvatarFallback>MA</AvatarFallback>
+        </Avatar>
+        <div class="flex flex-1 flex-col items-start">
+          <span class="text-sm">Misbah Ansori</span>
+          <span class="text-xs text-muted-foreground">misbah.ansori.dev</span>
+        </div>
+        <ChevronsUpDownIcon class="h-5 w-5" />
+      </Button>
+    </div>
+    <div class="flex flex-1 flex-col items-start gap-8 px-2 py-4">
+      <div
+        class="flex w-full flex-col items-start gap-1"
+        v-for="nav in navigations"
+      >
+        <span class="px-2 text-xs font-medium uppercase text-muted-foreground">
+          {{ nav.title }}
+        </span>
+        <div class="flex w-full flex-col items-start gap-2 pt-1">
+          <Button
+            :variant="activeNavigation === child.url ? 'secondary' : 'ghost'"
+            class="flex w-full items-center justify-start gap-2 px-3 text-left"
+            v-for="child in nav.children"
+            :key="child.url"
+          >
+            <Component :is="child.icon" class="w- h-5" />
+            <span class="text-sm">
+              {{ child.title }}
+            </span>
+          </Button>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-col items-start gap-1 px-2">
+      <Button
+        variant="ghost"
+        class="flex w-full items-center justify-start gap-2 px-3 text-left"
+      >
+        <MessageCircleQuestionIcon class="h-5 w-5" />
+        <span class="text-sm">Get Support</span>
+      </Button>
+      <Button
+        variant="ghost"
+        class="flex w-full items-center justify-start gap-2 px-3 text-left"
+      >
+        <MessageSquareTextIcon class="h-5 w-5" />
+        <span class="text-sm">Add Feedback</span>
+      </Button>
+
+      <Button
+        variant="ghost"
+        class="flex w-full items-center justify-start gap-2 px-3 text-left"
+      >
+        <span class="text-sm">Toggle Dark Mode</span>
+        <ChevronsUpDownIcon class="h-5 w-5" />
+      </Button>
+    </div>
+  </aside>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,7 @@
+<script setup lang="ts"></script>
 <template>
   <div>
     <nav class="flex h-20 items-center justify-between border-b"></nav>
-    <aside
-      class="fixed inset-y-0 left-0 flex min-h-screen w-[280px] flex-col border-r"
-    >
-      <div class="mx-2 h-20 px-2 py-4">
-        <Button variant>
-          <div>
-            <span>Misbah Ansori</span>
-            <span>misbah.ansori.dev</span>
-          </div>
-        </Button>
-      </div>
-      <div class="mx-2 flex flex-col items-start gap-1 px-2 py-4">
-        <Button size="sm" class="h-auto w-full justify-start py-2">
-          Dashboard
-        </Button>
-        <Button
-          size="sm"
-          variant="ghost"
-          class="h-auto w-full justify-start py-2"
-        >
-          Chart
-        </Button>
-        <Button
-          size="sm"
-          variant="ghost"
-          class="h-auto w-full justify-start py-2"
-        >
-          Payments
-        </Button>
-      </div>
-    </aside>
+    <Sidebar />
   </div>
 </template>
