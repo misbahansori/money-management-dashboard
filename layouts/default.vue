b8207f518410fa62d2e5846f8641d4db289a788b
Misbah Ansori <misbah.ansori24@gmail.com>
2024-02-09 15:09:22 +0700

    wip

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex">
+    <Sidebar />
+    <div class="flex min-h-screen flex-1 flex-col">
+      <Navbar />
+      <main class="flex-1 bg-accent">
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,50 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  ArrowUpRight,
+  BanknoteIcon,
+  CircleDollarSignIcon,
+  PackageIcon,
+  UserIcon,
+} from "lucide-vue-next";
+
+const cards = [
+  {
+    title: "Customer Unpaid",
+    icon: CircleDollarSignIcon,
+    value: 234,
+  },
+  {
+    title: "Total Revenue",
+    icon: BanknoteIcon,
+    value: 5673,
+  },
+  {
+    title: "Total Orders",
+    icon: PackageIcon,
+    value: 2344,
+  },
+  {
+    title: "Total Customers",
+    icon: UserIcon,
+    value: 7893,
+  },
+];
+</script>
 <template>
-  <div class="flex">
-    <Sidebar />
-    <div class="flex-1">
-      <Navbar />
-    </div>
+  <div class="grid grid-cols-4 gap-6 px-4 sm:p-6">
+    <Card v-for="card in cards" class="border-none">
+      <CardContent class="relative flex items-start gap-6 px-6 py-6">
+        <div class="rounded-md bg-accent p-2.5">
+          <Component :is="card.icon" class="h-6 w-6 text-muted-foreground" />
+        </div>
+        <div class="flex flex-col">
+          <span class="text-2xl font-medium tabular-nums">
+            {{ card.value }}
+          </span>
+          <span class="text-sm text-muted-foreground">{{ card.title }}</span>
+        </div>
+        <ArrowUpRight class="absolute right-4 top-4 h-5 w-5 text-muted" />
+      </CardContent>
+    </Card>
   </div>
 </template>
